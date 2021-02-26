7ed182440ed5197178aafb8923f9999e5b01c16d
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-02-26 13:09:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -45,9 +45,9 @@ function draw(){
     background(backgroundImg);
     text(mouseX+','+mouseY,30,45);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    //console.log(box2.body.position.x);
+    //console.log(box2.body.position.y);
+    //console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
